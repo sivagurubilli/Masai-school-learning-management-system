aa4a09d535a3ac9cb4a10ab128912c00da5b6e16
Siva  gurubilli <92457968+sivagurubilli@users.noreply.github.com>
2023-03-02 11:14:41 +0530

    Create Allroutes.tsx

---
+++ src/components/Allroutes.tsx
@@ -0,0 +1,49 @@
+import AdminLecture from "../Pages/AdminSidePages/AdminLecturePage/Index";
+import AdminSignup from "../Pages/AdminSidePages/AdminSignup/index";
+import React ,{useEffect,useState} from "react";
+import { Route, Routes } from "react-router-dom";
+import ForgetPassword from "../Pages/ForgotPassword/index";
+import Dashborad from "../Pages/StudentSidePages/StudentDashboard/Index";
+import AdminDashboard from "../Pages/AdminSidePages/AdminDashboard";
+import AdminLectureDetail from "../Pages/AdminSidePages/AdminLectureDetail/Index"
+import LandingPage from "../Pages/AdminSidePages/LandingPage";
+import PrivateRoute from "./PrivateRoutes";
+import AdminLectureCreate from "../Pages/AdminSidePages/CreateLecturePage/Index";
+import AdminLectureEdit from "../Pages/AdminSidePages/EditLecturePage/Index";
+import CreateBulkLecture from "../Pages/AdminSidePages/CreateBulkLecture/Index";
+import ResetPassword from "../Pages/ResetPassword/index"
+import Login from "../Pages/StudentSidePages/Login";
+import CopyLecture from "../Pages/AdminSidePages/CopyLecture/Index";
+import BookMarks from "../Pages/StudentSidePages/BookMarks/Index";
+import BookMarkDetails from "../Pages/StudentSidePages/BookMarkDetails/Index";
+
+const Allroutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path ="/" element ={<LandingPage />}/>
+       <Route path ="/admin/" element={<AdminDashboard/>}/>
+        <Route path="/admin/lectures"  element= { <AdminLecture />}/>
+        <Route path="/admin/lectures/:id" element={<AdminLectureDetail />} />
+        <Route path="/admin/lectures/edit/:id" element={<AdminLectureEdit />} />
+        <Route path="/student/dashboard" element={<Dashborad />} />
+        <Route path ="/admin/create-lectures" element ={<AdminLectureCreate/>}/>
+        <Route path ="/admin/dashboard" element ={<AdminDashboard />}/>
+        <Route path ="/admin/lectures/copy/:id" element ={<CopyLecture />}/>
+        <Route path="/admin/create-bulk-lectures" element ={<CreateBulkLecture/>}/>
+        <Route path="/user/profile" />
+        <Route path="/transcript" />
+        <Route path="/admin/signup" element={<AdminSignup />} />
+        <Route path="/admin/lectures/:id" element={<AdminLectureDetail />} />
+        <Route path="/student/dashboard" element={<Dashborad />} />
+        <Route path="/forgot-password" element={<ForgetPassword />} />
+        <Route path="/student/bookmarks" element={<BookMarks/>} />
+        <Route path="/student/bookmarks/:id" element={<BookMarkDetails />} />
+        <Route path ="/login" element ={<Login/>} />
+        <Route path="/reset-password" element={< ResetPassword/>} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Allroutes;
